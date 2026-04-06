cfb324271ddd1c50434481df144d123cad5030d3
Abhi Baweja <abaweja001@gmail.com>
2026-04-06 18:12:41 +0530

    Binomial Cofficient

--- methods.java
+++ methods.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class methods {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter any number");
-        long a = s.nextInt();
-        System.out.println("Factorial of "+ a +" is "+ fact(a));
+        System.out.print("Enter n: ");
+        long n = s.nextLong();
+        System.out.print("Enter r: ");
+        long r = s.nextLong();
+        System.out.println("Binomial cofficient of "+ n + " and " + r +" is "+ binomial(n,r));
     }
     public static long fact(long a){
         long n=1;
@@ -14,5 +16,13 @@ public class methods {
             a--;
         }
         return n;
+    }
+    public static long binomial(long n, long r){
+        long result;
+        long fact_n = fact(n);
+        long fact_r = fact(r);
+        long fact_nr = fact(n-r);
+        result = fact_n/(fact_nr * fact_r);
+        return result;
     }
 }
