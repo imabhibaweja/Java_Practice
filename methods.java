221d7b0f0118a353cede89a773ecd3f66ba784c6
Abhi Baweja <abaweja001@gmail.com>
2026-04-07 09:45:04 +0530

    Prime number function is added

--- methods.java
+++ methods.java
@@ -4,10 +4,17 @@ public class methods {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter n: ");
-        long n = s.nextLong();
-        System.out.print("Enter r: ");
-        long r = s.nextLong();
-        System.out.println("Binomial cofficient of "+ n + " and " + r +" is "+ binomial(n,r));
+        // long n = s.nextLong();
+        // System.out.print("Enter r: ");
+        // long r = s.nextLong();
+        // System.out.println("Binomial cofficient of "+ n + " and " + r +" is "+ binomial(n,r));
+        int a = s.nextInt();
+        boolean result = isPrime1(a);
+        if(result)
+            System.out.println("Its a prime number");
+        else
+            System.out.println("its not a prime number");
+        
     }
     public static long fact(long a){
         long n=1;
@@ -23,6 +30,16 @@ public class methods {
         long fact_r = fact(r);
         long fact_nr = fact(n-r);
         result = fact_n/(fact_nr * fact_r);
+        return result;
+    }
+    public static boolean isPrime1(int a){
+        boolean result = true;
+        for(int i=2;i<a;i++){
+            if(a%i==0){
+                result = false;
+                break;
+            }
+        }
         return result;
     }
 }
