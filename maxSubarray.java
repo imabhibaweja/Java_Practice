0291696e6075d0c70a7304ee39ae3eb1583a7f3c
Abhi Baweja <abaweja001@gmail.com>
2026-05-08 13:31:38 +0530

    maximum sum of subarray

--- maxSubarray.java
+++ maxSubarray.java
@@ -1,5 +1,24 @@
 public class maxSubarray{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int a[] = {1,-2,3,-5,6};
+        int curSum;
+        int maxSum=Integer.MIN_VALUE;
+        for(int i=0;i<a.length-1;i++){
+            for(int j=i+1;j<a.length;j++){
+                curSum=a[i];
+                System.out.print("("+a[i]);
+                for(int k=i+1;k<=j;k++){
+                    System.out.print(","+a[k]);
+                    curSum+=a[k];
+                }
+                System.out.print(")");
+                if(curSum>maxSum)
+                    maxSum=curSum;
+            }
+
+            System.out.println();
+        }
+        System.out.println("Maximum sum is "+ maxSum);
+       
     }
 }
