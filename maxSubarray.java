52d25dce00faef3a474dc2e3c1167c4edde74348
Abhi Baweja <abaweja001@gmail.com>
2026-05-08 14:14:13 +0530

    prefix method

--- maxSubarray.java
+++ maxSubarray.java
@@ -3,20 +3,31 @@ public class maxSubarray{
         int a[] = {1,-2,3,-5,6};
         int curSum;
         int maxSum=Integer.MIN_VALUE;
+        int prefix[]=new int[a.length];
+        prefix[0]=a[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+a[i];
+        }
         for(int i=0;i<a.length-1;i++){
-            for(int j=i+1;j<a.length;j++){
-                curSum=a[i];
-                System.out.print("("+a[i]);
-                for(int k=i+1;k<=j;k++){
-                    System.out.print(","+a[k]);
-                    curSum+=a[k];
+            for(int j=i;j<a.length;j++){
+                // curSum=a[i];
+                // System.out.print("("+a[i]);
+                // for(int k=i+1;k<=j;k++){
+                //     System.out.print(","+a[k]);
+                //     curSum+=a[k];
+                // }
+                // System.out.print(")");
+                if(i==0){
+                    curSum = prefix[j];
                 }
-                System.out.print(")");
+                else{
+                    curSum = prefix[j]-prefix[i-1];
+                }
                 if(curSum>maxSum)
                     maxSum=curSum;
             }
 
-            System.out.println();
+            // System.out.println();
         }
         System.out.println("Maximum sum is "+ maxSum);
        
