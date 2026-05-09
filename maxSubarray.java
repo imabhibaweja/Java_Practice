f3d3980c6a2d48a2e59ca87873a4c91d14665e93
Abhi Baweja <abaweja001@gmail.com>
2026-05-09 11:04:23 +0530

    prefix method is added

--- maxSubarray.java
+++ maxSubarray.java
@@ -2,7 +2,7 @@ public class maxSubarray{
     public static void main(String[] args) {
         int a[] = {1,-2,3,-5,6};
         bruteForce(a);
-        
+        prefix(a);
     }
     public static void bruteForce(int a[]){
         int curSum;
@@ -24,6 +24,30 @@ public class maxSubarray{
         }
         System.out.println("Maximum sum is "+ maxSum);
        
+    }
+    public static void prefix(int a[]){
+         int curSum;
+        int maxSum=Integer.MIN_VALUE;
+        int prefix[]=new int[a.length];
+        prefix[0]=a[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+a[i];
+        }
+        for(int i=0;i<a.length;i++){
+            for(int j=i;j<a.length;j++){
+                if(i==0){
+                    curSum=prefix[j];
+                }
+                else{
+                    curSum= prefix[j]-prefix[i-1];
+                }
+                if(maxSum<curSum){
+                    maxSum= curSum;
+                }
+            }  
+        }
+        System.out.println("Maximum sum is "+ maxSum);
+        
     }
     
 }
